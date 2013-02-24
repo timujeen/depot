a647101574eee87837dcb85a8718d80a5ab1ae5b
timujeen <rtimurb@mail.ru>
2013-02-24 17:18:28 +0400

    Validation

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	product = Product.new
+	assert product.invalid?
+	assert product.errors[:title].any?
+	assert product.errors[:descritption].any?
+	assert product.errors[:price].any?
+	assert product.errors[:img_url].any?
 end
